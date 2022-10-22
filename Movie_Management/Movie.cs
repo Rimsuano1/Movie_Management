a959ebb6c01be960e54352c40655ab5c4073e273
Rimsuano1 <huynhngochoangloc2002@gmail.com>
2022-10-22 23:47:36 +0700

    fix all project

---
+++ Movie_Management/Movie.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ManageMovie.Interface;
+
+namespace ManageMovie.Classes
+{   
+    public class Movie : IMovie
+    {
+        private int id;
+        private string name;
+        private string kind;
+        private string idFilm;
+        private string director;
+        private string filmClassification;
+        private string filmAdaptation;
+        private DateTime filmDuration;
+        private DateTime releaseDate;
+        private double tomatoPoints;
+
+        public Movie(int id, string name, string kind, string idFilm, string director, string filmClassification, string filmAdaptation, DateTime filmDuration, DateTime releaseDate, double tomatoPoints)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Kind = kind;
+            this.IdFilm = idFilm;
+            this.Director = director;
+            this.FilmClassification = filmClassification;
+            this.FilmAdaptation = filmAdaptation;
+            this.FilmDuration = filmDuration;
+            this.ReleaseDate = releaseDate;
+            this.TomatoPoints = tomatoPoints;
+        }
+
+        public Movie()
+        {
+        }
+
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Kind { get => kind; set => kind = value; }
+        public string Director { get => director; set => director = value; }
+        public string FilmClassification { get => filmClassification; set => filmClassification = value; }
+        public string FilmAdaptation { get => filmAdaptation; set => filmAdaptation = value; }
+        public DateTime FilmDuration { get => filmDuration; set => filmDuration = value; }
+        public DateTime ReleaseDate { get => releaseDate; set => releaseDate = value; }
+        public string IdFilm { get => idFilm; set => idFilm = value; }
+        public virtual double TomatoPoints { get => tomatoPoints; set => tomatoPoints = value; }
+        
+      
+        public void ViewListFilm()
+        {
+            var table = new Table();
+            table.SetHeaders("No", "ID Film", "Name", "Kind", "Director", "MPAA","Film Adaptation","Film Duration", "Release Date","Imdb");
+            table.AddRow($"{Id}", IdFilm, Name, Kind, Director, FilmClassification,FilmAdaptation,FilmDuration.ToString("hh:mm:ss"), ReleaseDate.ToString("dd/MM/yyyy"),TomatoPoints.ToString());
+            Console.WriteLine(table.ToString());
+
+        }
+
+       
+    }
+}
---
+++ Movie_Management/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using ManageMovie.Classes;
+using Movie_Management;
+
+namespace ManageMovie
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MenuFilm();
+        }
+        private static void MenuFilm()
+        {
+            int choice=0;
+            ManagementMovie manageMovie = new ManagementMovie();
+            manageMovie.FilmAdd();
+            string search;
+            
+            while (choice!=7)
+            {
+                Console.WriteLine("---------- RLMFlix ---------");
+                Console.WriteLine("1: Add New Film ");
+                Console.WriteLine("2: View List Film  ");
+                Console.WriteLine("3: Update information of Firm");
+                Console.WriteLine("4: Soft Film ");
+                Console.WriteLine("5: Search Film ");
+                Console.WriteLine("6: Delete Film ");
+                Console.WriteLine("7: Back to Main Menu");
+                Console.Write("Enter your choice: ");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        try
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            manageMovie.AddFilm();
+                          
+                        }
+                        catch (Exception e)
+                        {
+                            
+                        }
+                       
+                        break;
+                    case 2:
+                        manageMovie.ViewListFilm();
+                        break;
+                    case 3:
+                        Console.Write("Input Id film to search: ");
+                        search = Console.ReadLine();
+                        manageMovie.UpdateFilmByID(search);
+                        break;
+                    case 4:
+                        manageMovie.SoftFilmByID();
+                        break;
+                    case 5:
+                        Console.Write("Input Id film to search: ");
+                        search = Console.ReadLine();
+                        manageMovie.SearchFilmByID(search);
+                        break;
+                    case 6:
+                        Console.Write("Input Id film to search: ");
+                        search = Console.ReadLine();
+                        manageMovie.DeleteFilmByID(search);
+                        break;
+                   case 7:
+                       Console.WriteLine("-------------------------------");
+                        Console.WriteLine("Goodbye.");
+                        break;
+                }
+            }
+            
+        }
+        
+    }
+}
